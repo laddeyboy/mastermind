e77c3c1e465620df9d09e4fb116bd37baf47c88b
Josh Ladwig <joshua_ladwig@yahoo.com>
2019-01-05 08:41:38 -0600

    added constants file

---
+++ src/redux/constants.js
@@ -0,0 +1,26 @@
+export const SET_PLAYER_NAME = 'SET_PLAYER_NAME'
+export const TOGGLE_MAIN_MODAL = 'TOGGLE_MAIN_MODAL'
+export const SET_ACTIVE_COLOR = 'SET_ACTIVE_COLOR'
+export const SET_PEG_COLOR = 'SET_PEG_COLOR'
+export const SET_MARKER_COLOR = 'SET_MARKER_COLOR'
+export const INCREMENT_ATTEMPT = 'INCREMENT_ATTEMPT'
+export const SET_FINAL_SEQUENCE = 'SET_FINAL_SEQUENCE'
+export const TOGGLE_NEW_GAME = 'TOGGLE_NEW_GAME'
+export const TOGGLE_MARKER_TILE = 'TOGGLE_MARKER_TILE'
+export const SHOW_END_SCREEN = 'SHOW_END_SCREEN'
+export const SHOW_OBJECTIVES = 'SHOW_OBJECTIVES'
+export const RESET_GAME_STATE = 'RESET_GAME_STATE'
+
+
+export const gameBoard = [
+  ['white', 'white', 'white', 'white'],
+  ['white', 'white', 'white', 'white'],
+  ['white', 'white', 'white', 'white'],
+  ['white', 'white', 'white', 'white'],
+  ['white', 'white', 'white', 'white'],
+  ['white', 'white', 'white', 'white'],
+  ['white', 'white', 'white', 'white'],
+  ['white', 'white', 'white', 'white'],
+  ['white', 'white', 'white', 'white'],
+  ['white', 'white', 'white', 'white']
+]
